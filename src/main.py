e4dab8b6f07037e1ccc4e4a517e7fac61275ee74
csti-user <csti-user@csti-userdeMacBook-Pro.local>
2023-08-07 15:16:36 +0800

    fix(main.py,-t_pysql.py): showResult format

--- src/main.py
+++ src/main.py
@@ -6,43 +6,93 @@ import t_pysql
 from getmail import upLoadAttatchment
 from typing import List
 from pydantic import BaseModel
-
 import msal
 import uvicorn
 
 app = FastAPI()
-host = "0.0.0.0"
-port = 8777
+HOST = "0.0.0.0"
+PORT = 8777
 
-@app.get("/login")
-def login(request: Request):
-    # 初始化 MSAL
-    config = {
-        "client_id": getyamlkey('graph_client'),  # 從環境變數取得 client_id
-        "authority": "https://login.microsoftonline.com/common",
-        "redirect_uri": "http://localhost:8777/callback",  # 將回調地址改為你 FastAPI 服務的一個路由
-        "scope": ["https://graph.microsoft.com/.default"],
-    }
+# 以前的登入Azure區域
 
-    app = msal.PublicClientApplication(config['client_id'], authority=config['authority'])
+# @app.get("/login")
+# def login(request: Request):
+#     # 初始化 MSAL
+#     config = {
+#         "client_id": getyamlkey('graph_client'),  # 從環境變數取得 client_id
+#         "authority": "https://login.microsoftonline.com/common",
+#         "redirect_uri": "http://localhost:8777/callback",  # 將回調地址改為你 FastAPI 服務的一個路由
+#         "scope": ["https://graph.microsoft.com/.default"],
+#     }
 
-    # 建立登入的 URL 並重定向用戶到該 URL
-    auth_url = app.get_authorization_request_url(config['scope'], redirect_uri=config['redirect_uri'])
-    return RedirectResponse(auth_url)
+#     app = msal.PublicClientApplication(config['client_id'], authority=config['authority'])
 
+#     # 建立登入的 URL 並重定向用戶到該 URL
+#     auth_url = app.get_authorization_request_url(config['scope'], redirect_uri=config['redirect_uri'])
+#     return RedirectResponse(auth_url)
+
+# @app.get("/callback")
+# def callback(code: str):
+#     # 初始化 MSAL
+#     config = {
+#         "client_id": getyamlkey('graph_client'),
+#         "authority": "https://login.microsoftonline.com/common",
+#         "redirect_uri": "http://localhost:8777/callback",
+#         "scope": ["https://graph.microsoft.com/.default"],
+#     }
+
+#     app = msal.PublicClientApplication(config['client_id'], authority=config['authority'])
+
+#     # 使用授權碼獲取 token
+#     result = app.acquire_token_by_authorization_code(code, config['scope'], redirect_uri=config['redirect_uri'])
+    
+#     if "access_token" in result:
+#         # 成功取得 token，返回結果（或進行其他處理）
+#         access_token = result['access_token']
+        
+
+#         # TODO: 之後這邊應該會回傳到某一個地方
+#         return access_token
+#     else:
+#         # 取得 token 失敗，返回錯誤信息
+#         return HTTPException(
+#             status_code=HTTP_401_UNAUTHORIZED,
+#             detail=result.get('error_description', 'Unknown error'),
+#         )
+
+
+# 建立一個basemodel
 class Task(BaseModel):
     taskID: str
     token: str
     message_list: List[str]
 
+
 @app.post("/task")
-async def create_task(task: Task):
+async def create_task(task: Task) -> str:
+    """give sufficient information and it will upload specific attatchment to minIO
+
+    Args:
+        task (Task): contain taskID(str), token(str) and message_list(List[str])
+
+    Returns:
+        str: task id
+    """
     for message in task.message_list:
         upLoadAttatchment(token=task.token,taskID=task.taskID,MessageID=message)
-    return task
+    
+    return task.taskID
 
 @app.get("/checkTask")
-def checkTask(taskID:str):
+def checkTask(taskID:str) -> bool:
+    """check wheather the task is done, it check the column isbad == None in database
+
+    Args:
+        taskID (str): the task id specific on front-end
+
+    Returns:
+        bool: wheather the task is done
+    """
     rows = t_pysql.getTaskData(taskID)
     for row in rows:
         if row[2] == None:
@@ -51,39 +101,8 @@ def checkTask(taskID:str):
 
 @app.get("/showResult")
 def showResult(taskID:str):
-    rows = t_pysql.getyaraResult
+    rows = t_pysql.getyaraResult(taskID)
     return rows
 
-
-
-@app.get("/callback")
-def callback(code: str):
-    # 初始化 MSAL
-    config = {
-        "client_id": getyamlkey('graph_client'),
-        "authority": "https://login.microsoftonline.com/common",
-        "redirect_uri": "http://localhost:8777/callback",
-        "scope": ["https://graph.microsoft.com/.default"],
-    }
-
-    app = msal.PublicClientApplication(config['client_id'], authority=config['authority'])
-
-    # 使用授權碼獲取 token
-    result = app.acquire_token_by_authorization_code(code, config['scope'], redirect_uri=config['redirect_uri'])
-    
-    if "access_token" in result:
-        # 成功取得 token，返回結果（或進行其他處理）
-        access_token = result['access_token']
-        
-
-        # TODO: 之後這邊應該會回傳到某一個地方
-        return access_token
-    else:
-        # 取得 token 失敗，返回錯誤信息
-        return HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED,
-            detail=result.get('error_description', 'Unknown error'),
-        )
-
 if __name__ == "__main__":
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=HOST, port=PORT)
--- src/t_pysql.py
+++ src/t_pysql.py
@@ -1,8 +1,18 @@
 import mysql.connector
+from mysql.connector.connection import MySQLConnection
 from t_yamlReader import getyamlkey
 
-# 建立資料庫連線
-def create_connection():
+def create_connection() -> MySQLConnection:
+    """
+    Establish and return a connection to a MySQL database.
+
+    This function retrieves the database username and password from the 'getyamlkey' function
+    in the 't_yamlReader' module, and attempts to establish a connection to the 'outlookScan'
+    database on the 'db' host using this information.
+
+    Returns:
+        MySQLConnection: An open connection to the database.
+    """
     connection = mysql.connector.connect(
         host="db",
         user=getyamlkey('dbuser'),
@@ -31,7 +41,7 @@ def check_duplicate_id(id):
     conn.close()
     return result
 
-# 新增資料到資料庫
+# 新增mail資料到資料庫(原本的版本)
 def insert_maildata(MessageID, Subject, Received,Sender):
     conn = create_connection()
     cursor = conn.cursor()
@@ -47,6 +57,23 @@ def insert_maildata(MessageID, Subject, Received,Sender):
     cursor.close()
     conn.close()
 
+# 新增attatch資料到資料庫(原本的版本)
+def insert_attData(MessageID,AttatchmentName,ID):
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    query = "INSERT INTO attatchment (MessageID,AttatchmentName,ID) VALUES (%s,%s,%s)"
+    values = (MessageID,AttatchmentName,ID)
+
+    cursor.execute(query, values)
+    conn.commit()
+
+    print("Data inserted successfully.")
+
+    cursor.close()
+    conn.close()
+
+# 新增attatch資料（有task的版本）到資料庫
 def insert_attDataTask(MessageID,AttatchmentName,ID,TaskID):
     conn = create_connection()
     cursor = conn.cursor()
@@ -62,7 +89,7 @@ def insert_attDataTask(MessageID,AttatchmentName,ID,TaskID):
     cursor.close()
     conn.close()
 
-#把task的東西弄好
+# 取得attatchmentTask 這個表格符合TaskID的所有內容
 def getTaskData(TaskID):
     conn = create_connection()
     cursor = conn.cursor()
@@ -80,37 +107,25 @@ def getTaskData(TaskID):
     conn.close()
     return rows
 
-def getyaraResult(AttID):
+# 取得attatchmentTask跟yararesult JOIN的結果
+def getyaraResult(taskID):
     conn = create_connection()
     cursor = conn.cursor()
     # Query for 'isBad' = 1 and join with 'yara_result'
     is_bad_query = """
         SELECT yr.* 
-        FROM attatchmentTask yt 
-        JOIN yara_result yr ON yt.ID = yr.ID 
-        WHERE yt.isBad = %s
+        FROM attatchmentTask at 
+        JOIN yara_result yr ON at.ID = yr.ID 
+        WHERE at.isBad = 1 AND at.TaskID = %s
     """
-    cursor.execute(is_bad_query, (1,))
-    conn.close()
+    cursor.execute(is_bad_query, (taskID,))
+    rows = []
     for row in cursor:
-        print(row)
-    return cursor
-
-def insert_attData(MessageID,AttatchmentName,ID):
-    conn = create_connection()
-    cursor = conn.cursor()
-
-    query = "INSERT INTO attatchment (MessageID,AttatchmentName,ID) VALUES (%s, %s,%s)"
-    values = (MessageID,AttatchmentName,ID)
-
-    cursor.execute(query, values)
-    conn.commit()
-
-    print("Data inserted successfully.")
-
-    cursor.close()
+        rows.append(row)
     conn.close()
+    return rows
 
+# 更新資料
 def updateIsbad(id,value):
     conn = create_connection()
     cursor = conn.cursor()
@@ -126,6 +141,7 @@ def updateIsbad(id,value):
     cursor.close()
     conn.close()
 
+# 把yara結果放進yara_result表格
 def insert_scanResult(filename,match):
     conn = create_connection()
     cursor = conn.cursor()
